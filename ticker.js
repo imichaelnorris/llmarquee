4b77da7e3ffd5ddd91334a76308066dc3d95e5f2
Michael Norris <imichaelnorris@gmail.com>
2024-06-28 12:05:09 -0700

    init the ticket, fix imports

--- ticker.js
+++ ticker.js
@@ -1,17 +1,26 @@
-import { CreateMLCEngine } from "@mlc-ai/web-llm";
+import * as webllm from "https://esm.run/@mlc-ai/web-llm";
+import { CreateMLCEngine } from "https://esm.run/@mlc-ai/web-llm";
 
-// Callback function to update model loading progress
-const initProgressCallback = (initProgress) => {
-  console.log(initProgress);
-}
-const selectedModel = "Llama-3-8B-Instruct-q4f32_1-MLC";
 
-const engine = await CreateMLCEngine(
-  selectedModel,
-  { initProgressCallback: initProgressCallback }, // engineConfig
-);
+async function init() {
+  // Callback function to update model loading progress
+  const initProgressCallback = (initProgress) => {
+    console.log(initProgress);
+  }
+  const selectedModel = "TinyLlama-1.1B-Chat-v0.4-q4f16_1-MLC-1k";
+
+  const engine = await CreateMLCEngine(
+    selectedModel,
+    { initProgressCallback: initProgressCallback }, // engineConfig
+  );
+} 
 
 
 function llMarqueeCallback(prompt) {
 
 }
+
+window.addEventListener('load', async function() {
+  await init();
+})
+
